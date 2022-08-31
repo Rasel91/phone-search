8670b020e2053392e98913889e65c955e5041d26
Rasel91 <srasel.dpe@gmail.com>
2022-08-31 15:50:12 +0600

    add phone search

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,6 @@
 
-const loadPhone =async() =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async(searchText) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search= ${searchText}`;
     const res = await fetch(url);
     const  data = await res.json();
     displayPhones (data.data);
@@ -13,10 +13,10 @@ const displayPhones = phones => {
     const phoneDiv = document.createElement('div');
     phoneDiv.classList.add('col');
     phoneDiv.innerHTML=`
-    <div class="card">
+    <div class="card p-4">
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">${phone.phone_name}</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
   </div>
@@ -27,8 +27,14 @@ const displayPhones = phones => {
     
  });
 
-  console.log(phones);
+ 
 }
 
+document.getElementById('btn-search').addEventListener('click',function(){
+    const searhField = document.getElementById('search-field');
+    const searchText = searhField.value;
+    loadPhones(searchText);
 
-loadPhone ();
+})
+
+loadPhones();
